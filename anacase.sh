70a00ec97e0385f3e9db17d1fd4d80a310e1e330
César Bento Freire <cfreire@cfreire.com.pt>
2018-06-04 09:54:36 +0100

    Version 1.1.10

--- anacase.sh
+++ anacase.sh
@@ -3,16 +3,10 @@
 # anacase run script
 # 20180604 Cesar Freire
 
-DIRECTORY='anacase'
 REPO='https://github.com/cfreire/anacase.git'
 
-if [ ! -d "$DIRECTORY" ]; then
-	echo 'clonning anacase from ' "$REPO"
-	git clone "$REPO"
-fi
-	cd $DIRECTORY
-	echo 'sync github version'
-	git pull
-	echo 'startting anacase...'
-	./anacase.py
+echo 'sync github version'
+git pull
+echo 'startting anacase...'
+./anacase.py
 
