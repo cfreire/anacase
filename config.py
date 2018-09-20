faa372c8a4126562044b31b6e4e50b199c4d704e
César Bento Freire <cfreire70@gmail.com>
2018-09-20 11:15:38 +0100

    new config

--- config.py
+++ config.py
@@ -1,47 +1,44 @@
-__version__ = '1.0.1'
+"""Read configuration files"""
 
 import configparser
 import logging
 
+__version__ = '1.2.0'
 
-class Config:
-    """Read configuration files"""
+# ----------------------------------
+# internals
+_log = logging.getLogger(__name__)
+global key
+key = dict()
 
-    log = logging.getLogger(__name__)
 
-    def __init__(self, config_file_name):
-        self.config = configparser.RawConfigParser()
-        self.data = dict()
-        if not self.config.read(config_file_name):
-            msg = 'invalid configuration file "{}"'.format(config_file_name)
-            self.log.critical(msg)
-            raise ValueError(msg)
-        else:
-            self.log.info('success reading config file "{}"'.format(config_file_name))
+def init(config_file_name):
+    global _config
+    _config = configparser.RawConfigParser()
+    if not _config.read(config_file_name):
+        msg = 'invalid configuration file "{}"'.format(config_file_name)
+        _log.critical(msg)
+        raise ValueError(msg)
+    else:
+        _log.info('success reading config file "{}"'.format(config_file_name))
 
-    def section(self, section):
-        try:
-            self.data = dict(self.config.items(section))
-            self.log.debug('reading [{}] = {}'.format(section, self.data))
-            return self.data
-        except configparser.NoSectionError:
-            self.log.warning('section [{}] not found'.format(section))
-            return None
 
-    def key(self, key):
-        try:
-            self.log.debug('reading "{}" = {}'.format(key, self.data[key]))
-            return self.data[key]
-        except KeyError:
-            self.log.warning('key "{}" not found'.format(key))
-            return None
+def set_section(section):
+    try:
+        global key
+        key = dict(_config.items(section))
+        _log.debug('reading [{}] = {}'.format(section, key))
+        return key
+    except configparser.NoSectionError:
+        _log.warning('section [{}] not found'.format(section))
+        return None
 
 
 if __name__ == "__main__":
     logging.basicConfig()
     logging.getLogger().setLevel(logging.DEBUG)
-    cfg = Config('tests/config.ini')
-    cfg.section('GLOBAL')
-    assert cfg.key('log_level') == 'DEBUG'
+    init('tests/config.ini')
+    set_section('GLOBAL')
+    assert key['log_level'] == 'DEBUG'
 
 
