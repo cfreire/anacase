89046188a84823a4e96a0c7fb9bba1ecd42100d1
César Bento Freire <cfreire70@gmail.com>
2018-10-18 04:26:31 +0100

    versão 1.4.0

--- anacase.sh
+++ anacase.sh
@@ -4,8 +4,8 @@
 # 20180724 Cesar Freire
 
 echo 'sync github version'
-git checkout master
 git reset --hard HEAD~
+git checkout master
 git pull
 echo 'starting anacase...'
 /usr/bin/python3 anacase.py
